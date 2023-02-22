bba9a0606136cd8fb0a905ccd136e18e9f90d7aa
Joaopaulo922 <124464938+Joaopaulo922@users.noreply.github.com>
2023-02-22 09:08:00 -0300

    Update CalculadoraPY_Porcentagem.py

--- CalculadoraPY_Porcentagem.py
+++ CalculadoraPY_Porcentagem.py
@@ -1,23 +1,15 @@
-
-# programa para reallização de calculo de produtos em porcentagem, realiza um calculo por vez.
-
-var = input('Digite valor do produto, separado por um ponto "." .')
-z = float(var)
-soma = z * 0.32
-total = soma + z
-print(f'total com 32%, {total}')
+"""
+ programa para reallização de calculo de produtos em porcentagem, realiza um calculo por vez.
+ Programa com while, nunca vai ser interrompido. pois o valor não chegará a zero.
+"""
 
 
+valor = ''
+while valor != 0:
+    entrada_valor = float(input('Informe o valor do produto para realizar o calculo.?'))
+    entrada_porcentagem = float(input('Quantos porcentos deseja aplicar?'))
+    porcentagem = entrada_porcentagem / 100
+    total = entrada_valor + (porcentagem * entrada_valor)
 
-
-
-# Programa com while, nunca vai ser interrompido. pois o valor não chegará a zero.
-
-resposta = ''
-while resposta != 0:
-    resposta = input('Informe o valor do produto para realizar o calculo.?')
-    resposta1 = float(resposta)
-    soma = resposta1 * 0.45
-    total = soma + resposta1
-    print(f'Valor do produto é: {total}')
+    print(f'Valor do produto é: {total:.2f}\n')
 print(f'Obrigado por utilizar a aplicação!')
